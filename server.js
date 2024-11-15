c4611061500053f16055b4937ae6e0a81d27bffa
iginius-brando <brando@iginius.net>
2024-11-15 13:25:57 +0100

    Update server.js

--- server.js
+++ server.js
@@ -71,6 +71,6 @@ app.get('/', (req, res) => {
 });
 
 const port = process.env.PORT || 8080;
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {  // aggiungi '0.0.0.0'
   console.log(`Server listening on port ${port}`);
 });
